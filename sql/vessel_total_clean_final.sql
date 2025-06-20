d48474ccdd2fc4e1a13ab10d08295853076a1965
LERIOM <riom.antoin44@gmail.com>
2025-06-20 15:48:28 +0000

    add csv and sql

---
+++ sql/vessel_total_clean_final.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE IF NOT EXISTS vessel_total_clean_final (
+    id INTEGER,
+    mmsi BIGINT,
+    base_date_time TIMESTAMP,
+    lat DOUBLE PRECISION,
+    lon DOUBLE PRECISION,
+    sog DOUBLE PRECISION,
+    cog DOUBLE PRECISION,
+    heading DOUBLE PRECISION,
+    vessel_name TEXT,
+    imo TEXT,
+    call_sign TEXT,
+    vessel_type INTEGER,
+    status DOUBLE PRECISION,
+    length DOUBLE PRECISION,
+    width DOUBLE PRECISION,
+    draft DOUBLE PRECISION,
+    cargo DOUBLE PRECISION,
+    transceiver_class TEXT
+);
+
+ALTER TABLE vessel_total_clean_final ADD PRIMARY KEY (id);
+
+-- PostgreSQL bulk import
+\copy vessel_total_clean_final FROM 'vessel-total-clean-final.csv' DELIMITER ',' CSV HEADER;
+
+-- MySQL bulk import
+LOAD DATA INFILE '/path/to/vessel-total-clean-final.csv'
+INTO TABLE vessel_total_clean_final
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
