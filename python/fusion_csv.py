b34d179f801031c3afc9192b9b25cbffb8df5ba4
forsl4n <alban.lemaigat@orange.fr>
2025-06-24 09:56:57 +0000

    a

---
+++ python/fusion_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# === Fichiers ===
+base_cleaned_path = "/var/www/html/Projet_webA3/csv/vessel-total-clean-final.csv"  # <- Fichier contenant les données nettoyées
+clusters_path = "/var/www/html/Projet_webA3/python/resultats_clusters_150.csv"  # <- Fichier avec les 150 clusters
+output_path = "data_base_150_with_clusters.csv"  # <- Fichier final
+
+# === Chargement des fichiers ===
+df_cleaned = pd.read_csv(base_cleaned_path)
+df_clusters = pd.read_csv(clusters_path)
+
+# === Fusion des deux tables sur la colonne 'mmsi'
+df_final = df_cleaned.merge(df_clusters[['mmsi', 'cluster_kmeans']], on='mmsi', how='left')
+
+# === Sauvegarde du fichier final
+df_final.to_csv(output_path, index=False)
+print(f"✅ Fusion réussie. Fichier enregistré sous : {output_path}")
